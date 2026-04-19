4a87735479892850f620c623a066585b33a0086d
shayne <79330+shayne@users.noreply.github.com>
2026-04-19 16:10:35 -0400

    build: add iOS app scaffold

---
+++ apple/Derphole/Derphole/DerpholeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DerpholeApp.swift
+//  Derphole
+//
+//  Created by Shayne Sweeney on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct DerpholeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
